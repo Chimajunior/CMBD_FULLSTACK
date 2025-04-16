c059dd06a5cbc556b6522a63d5901b54687e1ac3
chima <chimajunior14@gmail.com>
2025-04-16 01:55:38 +0100

    new db

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+// const mysql = require('mysql2');
+// require('dotenv').config();
 
 // const pool = mysql.createPool({
 //   host: process.env.DB_HOST,
@@ -9,22 +9,15 @@ require('dotenv').config();
 //   database: process.env.DB_NAME,
 // }).promise();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306, // Make sure this line is here
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-}).promise();
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT || 3306, // Make sure this line is here
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// }).promise();
 
 
-pool.getConnection()
-  .then(() => console.log("✅ Connected to MySQL!"))
-  .catch(err => {
-    console.error("❌ MySQL connection failed:", err.message);
-  });
-
-module.exports = pool;
 
 
 // const mysql = require('mysql2');
@@ -38,8 +31,29 @@ module.exports = pool;
 //     database: process.env.DB_NAME
 // }).promise(); // Enables async/await
 
+import mysql from 'mysql2/promise';
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT|| 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+export default pool;
 
 
+// pool.getConnection()
+//   .then(() => console.log("✅ Connected to MySQL!"))
+//   .catch(err => {
+//     console.error("❌ MySQL connection failed:", err.message);
+//   });
+
+// module.exports = pool;
 
 
 
